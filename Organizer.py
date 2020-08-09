ce2816eeda6ea18fda7bb46b84e8662d66186613
Rubenz00 <61566859+Rubenz00@users.noreply.github.com>
2020-08-09 19:33:19 +0200

    Add files via upload

---
+++ Organizer.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+SUBDIRECTORIES = {
+    "PNG": [".png"] ,
+    "JPG": [".jpg",".jpeg"]
+}
+def pickDirectory (value):
+    for category, suffixes in SUBDIRECTORIES.items():
+        for suffix in suffixes:
+            if suffix == value:
+                return category
+    return "MISC "
+
+def organizeDirectory ():
+    for item in os.scandir():
+        if item.is_dir():
+            continue
+        filePath = Path(item)
+        filetype = filePath.suffix.lower()
+        directory = pickDirectory (filetype)
+        directoryPath = Path(directory)
+        if directoryPath.is_dir() != True:
+            directoryPath.mkdir()        
+        filePath.rename(directoryPath.joinpath(filePath))
+
+organizeDirectory()
